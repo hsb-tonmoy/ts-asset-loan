b77d138c8c01a6a5c779a385d6069a4c685e71d6
Hasibuzzaman Tonmoy <hsb.tonmoy@gmail.com>
2023-11-30 19:52:52 -0500

    requestpreview

--- src/routes/(admin)/admin/request/[id]/RequestPreview.svelte
+++ src/routes/(admin)/admin/request/[id]/RequestPreview.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import Label from '$lib/components/ui/label/label.svelte';
 
 	export let data: PageData;
+
+	const { request } = data;
 </script>
+
+<div class="flex flex-col gap-6">
+	<div class="flex">
+		<img src="/img/avatars/male.svg" alt={request?.firstName} class="w-20 rounded-full" />
+		<div class="flex flex-col">
+			<Label>{request?.firstName + request?.lastName}</Label>
+		</div>
+	</div>
+</div>
